f1e2f0838aa7d06ea22285ce0af13704dd2237b0
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-02 11:06:51 -0400

    completed looping from 1 to 10 exercise

--- python-exercise7.py
+++ python-exercise7.py
@@ -1,2 +1,7 @@
 # Loop from 1-10 and print each number
-counter = 0
+# Set loop counter and result variable, can be the same variable in this case
+num = 1
+
+while num <= 10:
+    print(num)
+    num += 1
