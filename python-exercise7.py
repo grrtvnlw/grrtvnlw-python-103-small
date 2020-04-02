ceb8f1ea2b7b93a437fe53b55044a2e93dbd4ba7
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-02 11:15:07 -0400

    renamed files

--- python-exercise7.py
+++
@@ -1,8 +0,0 @@
-# Loop from 1-10 and print each number
-# Set loop counter and result variable, can be the same variable in this case
-num = 1
-
-# Logic
-while num <= 10:
-    print(num)
-    num += 1
--- python-exercise8.py
+++
@@ -1,9 +0,0 @@
-# Loop from 1-10 and print each number
-# Get user input for start num and end num / must convert input to int
-start_num = int(input("Start from: "))
-end_num = int(input("End on: "))
-
-# Logic
-while start_num <= end_num:
-    print(start_num)
-    start_num += 1
